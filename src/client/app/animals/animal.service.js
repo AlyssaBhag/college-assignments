d39690e6c56221aaccdfe8befe2187ce81e91099
3 <167788887+AlyssaBhag@users.noreply.github.com>
2025-01-08 19:43:42 -0500

    There are my updates to the client src folder

---
+++ src/client/app/animals/animal.service.js
@@ -0,0 +1,7 @@
+/*
+Name: Alyssa Bhagwandin
+Filename: animal.service.js
+Course: INFT 2202
+Created Date: January 8th, 2024
+Description: This is my animal.service.js file
+*/
---
+++ src/client/app/animals/search.js
@@ -0,0 +1,7 @@
+/*
+Name: Alyssa Bhagwandin
+Filename: search.js
+Course: INFT 2202
+Created Date: January 8th, 2024
+Description: This is my search.js file
+*/
