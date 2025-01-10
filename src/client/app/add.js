a991e55ea4404082a03d46d8eb771d57c029fd25
3 <167788887+AlyssaBhag@users.noreply.github.com>
2025-01-10 18:43:22 -0500

    Add files via upload

---
+++ src/client/app/add.js
@@ -0,0 +1,4 @@
+// Keeping this here for a little.
+const date = new date();
+const copy = document.getElementById('copyright');
+copy.append(date.getFullYear)
--- src/client/app/animals/animal.service.js
+++ src/client/app/animals/animal.service.js
@@ -5,3 +5,4 @@ Course: INFT 2202
 Created Date: January 8th, 2024
 Description: This is my animal.service.js file
 */
+
